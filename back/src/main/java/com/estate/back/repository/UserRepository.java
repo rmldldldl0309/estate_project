0910421a4db63c5dcd7daf29cd1bae399ab2d567
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-24 11:54:17 +0900

    리포지토리 생성

---
+++ back/src/main/java/com/estate/back/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.estate.back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.estate.back.entity.UserEntity;
+
+// estate 데이터베이스의 user테이블의 작업을 위한 리포지토리
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    
+}
