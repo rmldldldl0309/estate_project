013b45c46f2d941e38703ebd1ca8347f7f70b93b
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-24 14:55:49 +0900

    HTTP code

---
+++ back/src/main/java/com/estate/back/dto/response/ResponseCode.java
@@ -0,0 +1,32 @@
+package com.estate.back.dto.response;
+
+/*
+200 성공: SU / Success.
+
+400 필수 데이터 미입력 : VF / Validation Failed.
+400 중복된 아이디 입력 : DI / Duplicated Id.
+400 중복된 이메일 입력 : DE / Duplicated Email.
+401 로그인 정보 불일치 : SF / Sign in Failed.
+401 인증 실패 : AF / Authentication Failed
+
+500 이메일 전송 실패 : MF / Mail send Failed.
+500 토큰 생성 실패 : TF / Token creation Failed.
+500 데이터베이스 오류: DBE / Database Error.
+ */
+
+// Response의 공통된 code값들
+public interface ResponseCode {
+
+    String SUCCESS = "SU";
+
+    String VALIDATION_FAILED = "VF";
+    String DUPLICATED_ID = "DI";
+    String DUPLICAT_EMAIL = "DE";
+    String SIGN_IN_FAILED = "SF";
+    String AUTHENTICATION_FAILED = "AF";
+
+    String MAIL_SEND_FAILED = "MF";
+    String TOKEN_CREATION_FAILED = "TF";
+    String DATABASE_ERROR = "DBE";
+    
+}
