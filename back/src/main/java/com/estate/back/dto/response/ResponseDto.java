d34f1925aabe9095f84aea2d0ebda9e8b0177509
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-24 14:56:25 +0900

    공통형태

---
+++ back/src/main/java/com/estate/back/dto/response/ResponseDto.java
@@ -0,0 +1,73 @@
+package com.estate.back.dto.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// Response의 공통형태
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String code;
+    private String message;
+
+    // 클래스로 직접 메서드 호출 > static
+    public static ResponseEntity<ResponseDto> success() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+    
+    public static ResponseEntity<ResponseDto> validationFailed() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.VALIDATION_FAILED, ResponseMessage.VALIDATION_FAILED);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> duplicatedId() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.DUPLICATED_ID, ResponseMessage.DUPLICATED_ID);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> duplicatedEmail() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.DUPLICAT_EMAIL, ResponseMessage.DUPLICATED_EMAIL);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> signInFailed() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.SIGN_IN_FAILED, ResponseMessage.SIGN_IN_FAILED);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> authenticationFailed() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.AUTHENTICATION_FAILED, ResponseMessage.AUTHENTICATION_FAILED);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> mailSendError() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.MAIL_SEND_FAILED, ResponseMessage.MAIL_SEND_FAILED);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> tokenCreationFailed() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.TOKEN_CREATION_FAILED, ResponseMessage.TOKEN_CREATION_FAILED);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto responseBody = 
+            new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+}
