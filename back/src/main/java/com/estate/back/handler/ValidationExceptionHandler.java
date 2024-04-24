35393b99e6984a0f23f8d8a6651cbfb76bd10955
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-24 17:43:48 +0900

    데이터 유효성검사 예외 처리

---
+++ back/src/main/java/com/estate/back/handler/ValidationExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.estate.back.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.estate.back.dto.response.ResponseDto;
+
+// Request의 데이터 유효성 검사에서 발생하는 예외 처리
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+    
+    // RequestBody의 데이터 유효성 검사 중 발생하는 예외 핸들링
+    // - MethodArgumentNotValidException : 유효하지 않은 데이터
+    // - HttpMessageNotReadableException : RequestBody가 없어서 유효성 검사를 못할 때
+    @ExceptionHandler({
+        MethodArgumentNotValidException.class,
+        HttpMessageNotReadableException.class
+    })
+    public ResponseEntity<ResponseDto> validationExceptionHandler(
+        Exception exception
+    ) {
+        exception.printStackTrace();
+        return ResponseDto.validationFailed();
+    }
+
+}
+
+/*
+응답 : 실패 (필수 데이터 미입력)
+HTTP/1.1 400 Bad Request
+Content Type: application/json;charset=UTF-8
+{
+    "code": "VF"
+    "message": "Validation Failed"
+}
+
+- {}: ResponseDto
+- 전체 : ResponseEntity
+ */
