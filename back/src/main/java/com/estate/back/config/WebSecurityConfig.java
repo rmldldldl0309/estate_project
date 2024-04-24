1b8599656c0811266c75cffe6c197a4feaa7491a
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-24 12:43:15 +0900

    JwtAuthenticationFilter 추가

--- back/src/main/java/com/estate/back/config/WebSecurityConfig.java
+++ back/src/main/java/com/estate/back/config/WebSecurityConfig.java
@@ -9,9 +9,14 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.estate.back.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 /*
 ▶ Spring Web Security 설정
@@ -19,13 +24,18 @@ Basic 인증 미사용
 CSRF 정책 미사용
 Session 생성 정책 미사용
 CORS 정책 (모든 출처 - 모든 메서드 - 모든 패턴 허용)
+
+JwtAuthenticationFilter 추가 (UsernamePasswordAuthenticationFilter 이전에 추가)
 */
 
 // 등록, 수정, web security 설정 지원
 @Configurable
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception{
@@ -39,7 +49,9 @@ public class WebSecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             // 아래에서 작성한 CORS정책 설정 적용
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            // JwtAuthenticationFilter 추가 (UsernamePasswordAuthenticationFilter 이전에 추가)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
