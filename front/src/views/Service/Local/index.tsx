0ab2cf303b6a8fa104c8571a533d6cfd4c1c4f11
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-26 17:37:53 +0900

    임시

---
+++ front/src/views/Service/Local/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+export default function Local() {
+    return (
+        <div>Local</div>
+    )
+}
---
+++ front/src/views/Service/Ratio/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+export default function Ratio() {
+    return (
+        <div>Ratio</div>
+    )
+}
