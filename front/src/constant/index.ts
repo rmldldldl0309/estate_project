14a31eb3a566ab3f86d5df99d4d987afdb460626
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-26 12:48:56 +0900

    URL 경로 지정

---
+++ front/src/constant/index.ts
@@ -0,0 +1,37 @@
+// description : URL PATH
+
+export const AUTH_PATH = '/authentication';
+
+export const SERVICE_PATH = '/service';
+export const LOCAL_PATH = 'local';
+export const RATIO_PATH = 'ratio';
+export const QNA_PATH = 'qna';
+
+export const QNA_WRITE_PATH = 'write'
+export const QNA_DETAIL_PATH = ':rececptionNumber'
+export const QNA_UPDATE_PATH = 'update/:rececptionNumber'
+
+// description : Navigation 절대 URL PATH
+
+export const AUTH_ABSOULUTE_PATH = AUTH_PATH;
+
+export const LOCAL_ABSOULUTE_PATH = `${SERVICE_PATH}/${LOCAL_PATH}`;
+export const RATIO_ABSOULUTE_PATH = `${SERVICE_PATH}/${RATIO_PATH}`;
+export const QNA_LIST_ABSOULUTE_PATH = `${SERVICE_PATH}/${QNA_PATH}`;
+
+export const QNA_WRITE_ABSOULUTE_PATH = `${SERVICE_PATH}/${QNA_PATH}/${QNA_WRITE_PATH}`;
+export const QNA_DETAIL_ABSOULUTE_PATH = (rececptionNumber: string) => `${SERVICE_PATH}/${QNA_PATH}/${rececptionNumber}`
+export const QNA_UPDATE_ABSOULUTE_PATH = (rececptionNumber: string) => `${SERVICE_PATH}/${QNA_PATH}/update/${rececptionNumber}`
+
+// description : API URL PATH
+
+export const SERVER_DOMAIN_URL = 'http://localhost4000';
+export const SERVER_API_URL = `${SERVER_DOMAIN_URL}/api/v1`;
+export const SERVER_AUTH_MODULE_URL = `${SERVER_API_URL}/auth`;
+
+export const SIGN_IN_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-in`;
+export const ID_CHECK_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/id-check`;
+export const EMAIL_AUTH_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email-auth`;
+export const EMAIL_AUTH_CHECK_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email-auth-check`
+export const SIGN_UP_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-up`;
+
